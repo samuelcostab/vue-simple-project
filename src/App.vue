f416741ec00c4dd22632a71b447fc0fb569c8cae
samuelcostab <samuelufcbrito@gmail.com>
2019-11-22 08:15:23 -0300

    testando bootstrap

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <PaginaPrincipal msg="Primeira aplicação em Vue"/>
+  </div>
+</template>
+
+<script>
+import PaginaPrincipal from './components/PaginaPrincipal'
+
+export default {
+  name: 'app',
+  components: {
+    PaginaPrincipal
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
